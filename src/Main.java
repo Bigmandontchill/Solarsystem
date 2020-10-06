b907075cdaf076ee55543928594ab00b0bbb0b2a
B <tianranwang20000@gmail.com>
2020-10-06 11:09:19 +0100

    third push

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,13 @@
 public class Main {
     public static void main(String[] args) {
+        int angle=50;
         System.out.println("Hello World!");
-   //     SolarSystem solar=new SolarSystem(800,800);
-
+       SolarSystem solar=new SolarSystem(800,800);
+       while (true) {
+           solar.drawSolarObject(30, 0, 40, "YELLOW");
+           solar.drawSolarObjectAbout(300,angle, 20, "BLUE", 30, 0);
+           angle+=2;
+           solar.finishedDrawing();
+       }
     }
 }
