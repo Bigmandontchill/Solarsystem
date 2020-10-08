b0d95504b48f8d2eec21f07e6444c3feac583a0c
B <tianranwang20000@gmail.com>
2020-10-08 13:01:59 +0100

    Added comments

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        int angle=50;
+        double angle=50;
         System.out.println("Hello World!");
        SolarSystem solar=new SolarSystem(800,800);
        while (true) {
-           solar.drawSolarObject(30, 0, 40, "YELLOW");
-           solar.drawSolarObjectAbout(300,angle, 20, "BLUE", 30, 0);
-           angle+=2;
+           solar.drawSolarObject(30, 0, 100, "YELLOW");// create the sun
+           //create the earth
+           solar.drawSolarObjectAbout(300,angle, 40, "BLUE", 30, 0);
+           angle+=1;//make the earth orbit around the sun 
            solar.finishedDrawing();
        }
     }
