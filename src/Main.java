cccf14d02ed2891ac4da13f81c59a637caad3eb5
bigmandontchill <t.wang14@lancaster.ac.uk>
2020-10-13 14:45:09 +0100

    Added new classes Planet,Sun.

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,19 @@
 public class Main {
     public static void main(String[] args) {
-        double angle=0;
-        double angleTwo=0;
         System.out.println("Hello World!");
-       SolarSystem solar=new SolarSystem(800,800);
+       SolarSystem system=new SolarSystem(800,800);
+        Sun sun=new Sun(90,-300,60,"YELLOW",system.getWidth()/2,system.getHeight()/2);
+       Planet planet=new Planet(0,50,30,"RED",0,0);
+      /*  double centrerads = Math.toRadians(sun.angle);
+        double centreOfRotationX = ( 800/ 2.0) + sun.r * Math.sin(centrerads);
+        double centreOfRotationY = (800/ 2.0) +sun.r * Math.cos(centrerads);
+        planet.CenterX=centreOfRotationX;
+        planet.Centery=centreOfRotationY;*/
        while (true) {
-           solar.drawSolarObject(0, 0, 100, "YELLOW");// create the sun
-           //create the earth
-           angle+=0.3;
-           angleTwo+=0.5;
-           solar.drawSolarObjectAbout(150,angle, 40, "BLUE", 0, 0);
-           solar.drawSolarObjectAbout(300,angleTwo, 40, "RED", 0, 0);
-
-           solar.finishedDrawing();
+           system.Draw(sun);
+           planet.move(-1,sun,system);
+           system.Draw(planet);
+           system.finishedDrawing();
        }
 
 
---
+++ src/SolarObject.java
@@ -0,0 +1,25 @@
+import java.awt.*;
+import java.awt.event.WindowEvent;
+
+public  class SolarObject {
+
+    protected double angle;
+    protected double r;
+    protected int diameter;
+    protected String col;
+    protected double CenterX;
+    protected double Centery;
+
+    public SolarObject(double angle, double r, int diameter, String col,double CenterX,double Centery) {
+        this.angle = angle;
+        this.r = r;
+        this.diameter = diameter;
+        this.col = col;
+        this.CenterX=CenterX;
+        this.Centery=Centery;
+    }
+
+
+
+    }
+
